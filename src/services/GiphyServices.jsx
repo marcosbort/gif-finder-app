949f4a11d465c871fd3ad24d1cb4d1a1d980d685
Marcos Bort <marcosbort@gmail.com>
2022-05-12 16:59:36 -0300

    code update details

--- src/services/GiphyServices.jsx
+++ src/services/GiphyServices.jsx
@@ -11,7 +11,6 @@ export const getGifs = async (keyword) => {
       const gifs = data.map((image) => {
         const { title, id } = image
         const url = image.images.preview_webp.url
-        //const url = image.images.fixed_height.url
         const urlHD = image.images.downsized.url
         return { title, id, url, urlHD }
       })
@@ -29,7 +28,6 @@ export const getGifById = async (keyword, id) => {
       const gifs = data.map((image) => {
         const { title, id } = image
         const url = image.images.preview_webp.url
-        // const url = image.images.fixed_height.url
         const urlHD = image.images.downsized.url
         return { title, id, url, urlHD }
       })
@@ -47,7 +45,6 @@ export const getTrendingGifs = async () => {
       const trendingGifs = data.map((image) => {
         const { title, id } = image
         const url = image.images.preview_webp.url
-        //const url = image.images.fixed_height.url
         const urlHD = image.images.downsized.url
         return { title, id, url, urlHD }
       })
